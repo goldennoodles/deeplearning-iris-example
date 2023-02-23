a893c3656504d52aeacc246aaa370e46b8148e42
goldennoodles <flickydabeany@gmail.com>
2023-02-23 17:09:42 +0000

    added initial example with tests to run

---
+++ src/main/java/org/noodles/deeplearning/Iris.java
@@ -0,0 +1,31 @@
+package org.noodles.deeplearning;
+
+public class Iris {
+    private float sepalLength;
+    private float sepalWidth;
+    private float petalLength;
+    private float petalWidth;
+
+    public Iris(float sepalLength, float sepalWidth, float petalLength, float petalWidth) {
+        this.sepalLength = sepalLength;
+        this.sepalWidth = sepalWidth;
+        this.petalLength = petalLength;
+        this.petalWidth = petalWidth;
+    }
+
+    public float getSepalLength() {
+        return sepalLength;
+    }
+
+    public float getSepalWidth() {
+        return sepalWidth;
+    }
+
+    public float getPetalLength() {
+        return petalLength;
+    }
+
+    public float getPetalWidth() {
+        return petalWidth;
+    }
+}
